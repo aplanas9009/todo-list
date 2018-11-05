2a5cbb1c57bb165f31ffd7c185588d40ae826323
af <af@aleph.engineering>
2018-11-04 20:49:14 -0500

    Generate tasks model

---
+++ src/app/tasks/model/mock-tasks.ts
@@ -0,0 +1,14 @@
+import { Task } from './task';
+
+export const TASKS: Task[] = [
+  { id: 11, name: 'Find the soap' },
+  { id: 12, name: 'wash the dog' },
+  { id: 13, name: 'charge the iphone' },
+  { id: 14, name: 'Go to church' },
+  { id: 15, name: 'Seduce miss Brissete' },
+  { id: 16, name: 'Find the magazines' },
+  { id: 17, name: 'Go to gym' },
+  { id: 18, name: 'Talk with my friends' },
+  { id: 19, name: 'Finish the flintstones' },
+  { id: 20, name: 'Avoid the Tornado' }
+];
